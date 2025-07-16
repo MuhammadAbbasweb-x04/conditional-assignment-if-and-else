6194db4b7c55349cf4446024a18d2d9b563dfeb2
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-07-16 14:04:34 +0500

    5 QUTIONS COMPLETED

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+// Q1 Ask the user for their age. If the age is 18 or more, show an alert:
+
+// Answer
+// var age = 18;
+// if (age >=18 && age <=25){
+// alert("Your are an adult");
+// }else{
+//     alert("Your are a minor");
+// }
+
+// Q2 Ask the user to enter a number. If it's positive, log:
+
+// Answer
+// var enternumber=4;
+// if( enternumber>= 4 && enternumber<=10 ){
+// console.log("Positive number");
+// }else{
+//     console.log(" Not Positive");
+// }
+
+// Q3 Ask the user for a number. If it’s even, alert:
+
+// Answer
+// var enternumber= 2;
+// if ( enternumber >= 2 && enternumber <= 4){
+//     alert(" Even number ");
+// }else{
+//     alert(" odd number");
+// }
+// 
+
+// Q4 Ask for a username. If the username is admin:
+
+// Answer
+// var admin = prompt("Enter your name");
+// if(admin == "admin"){
+//     alert("Welcome admin!");
+// }
+// else{
+//     alert("Access denied.");
+// }
+
+// Q5 Ask for two numbers. If they are equal, log
+
+// Answer
+// var num1 =prompt ( " Enter first number" );
+// var num2 =prompt( " Enter second number") ;
+// if(num1 == num2){
+// console.log("Both numbers are the same");    
+// }else{
+//    console.log("The numbers are different"); 
+// }
+
